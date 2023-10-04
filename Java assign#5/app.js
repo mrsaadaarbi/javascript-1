3a965af042c749e82b7561d6333565343760c60c
mrsaadaarbi <145856837+mrsaadaarbi@users.noreply.github.com>
2023-10-04 07:20:47 -0700

    Add files via upload

---
+++ Java assign#5/app.js
@@ -0,0 +1,47 @@
+// Chapter no: 21
+// Q1
+// var userInput=prompt()
+// var userInput=userInput.toLowerCase
+// Q2
+// var userInput=prompt()
+// userInput=userInput.toLowerCase
+// var alphabet="X"
+// Q3
+// var userInput=prompt()
+// userInput=userInput.toUperCase
+// var alphabet="y"
+// Q7
+// var userInput=prompt("Where do you live")
+// var firstChar=userInput.slice(0,1).toUpperCase();
+// var remainChar=userInput.slice(1).toLowerCase();
+// var combineInput=firstChar+remainChar;
+// var cityArr=["Karachi","Hyderabad","Lahore","Multan"]
+// for(var i=0;i<cityArr.length;i++){
+//     if(combineInput===cityArr[i]){
+//         console.log("Match")
+//     }
+// }
+          //  Chapter No:22-25
+// // Q10
+// var alpha="a,b,c,d,e"
+// the index no 2 is c.
+// Q9
+// var arr=["123","abc","bcd"]
+// if(arr==="abc"){
+//     console.log("match")
+// }
+// Q16
+// var str="1"
+// var newStr="one"
+// console.log(newStr.replace(str))
+
+// Q13
+
+// Cha=["lion","cheeta","elephant","Monkey","donkey"]
+// var animalNames=Cha
+// console.log(animalNames)
+
+// Q17
+// var Alphabets=("a,b,c")
+// var y=("a,j,z")
+// console.log(y.replace(Alphabets))
